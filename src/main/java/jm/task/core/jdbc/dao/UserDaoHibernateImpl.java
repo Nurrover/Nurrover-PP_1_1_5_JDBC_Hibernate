247ac629aa03d1a7d5219a9984f4f3d2c5a9cdf6
Nurrover <nuriksafarov00@gmail.com>
2023-11-10 02:40:56 +0400

    Hibernate version 3.0

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -11,21 +11,41 @@ public class UserDaoHibernateImpl implements UserDao {
 
     private final SessionFactory sessionFactory = getSessionFactory();
 
-    public UserDaoHibernateImpl() {
-
-    }
+    public UserDaoHibernateImpl() { }
 
 
     @Override
     public void createUsersTable() {
-        Session session = sessionFactory.openSession();
-
-        try (session) {
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
             session.createSQLQuery(
                     "CREATE TABLE IF NOT EXISTS Users (id bigint AUTO_INCREMENT PRIMARY KEY , name varchar(32), lastName varchar(32), age tinyint)"
             ).executeUpdate();
+
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void dropUsersTable() {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            session.createSQLQuery("DROP TABLE IF EXISTS Users").executeUpdate();
+
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        Session session = sessionFactory.openSession();
+
+        try (session) {
+            session.beginTransaction();
+
+            session.persist(new User(name, lastName, age));
 
             session.getTransaction().commit();
         } catch (Exception e) {
@@ -34,39 +54,17 @@ public class UserDaoHibernateImpl implements UserDao {
     }
 
     @Override
-    public void dropUsersTable() {
+    public void removeUserById(long id) {
         Session session = sessionFactory.openSession();
 
         try (session) {
             session.beginTransaction();
 
-            session.createSQLQuery("DROP TABLE IF EXISTS Users").executeUpdate();
-
-            session.getTransaction().commit();
-        }  catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-    }
-
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-
-            session.persist(new User(name, lastName, age));
-
-            session.getTransaction().commit();
-        }
-    }
-
-    @Override
-    public void removeUserById(long id) {
-        try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-
             session.remove(session.get(User.class, id));
 
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
         }
     }
 
